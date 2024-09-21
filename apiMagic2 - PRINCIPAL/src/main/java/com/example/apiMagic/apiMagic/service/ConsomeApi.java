6ea241981e19b905f284e78f0e094edc57e20626
Yvesmorello <124888523+Yvesmorello@users.noreply.github.com>
2024-09-21 14:52:13 -0300

    Update ConsomeApi.java

--- apiMagic2 - PRINCIPAL/src/main/java/com/example/apiMagic/apiMagic/service/ConsomeApi.java
+++ apiMagic2 - PRINCIPAL/src/main/java/com/example/apiMagic/apiMagic/service/ConsomeApi.java
@@ -2,31 +2,31 @@ package com.example.apiMagic.apiMagic.service;
 
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.stream.Collectors;
 
 @Component
 public class ConsomeApi {
 
-    public String obterDados(String endereco) {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = null;
-        try {
-            response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    private final HttpClient httpClient = HttpClient.newHttpClient();
 
-        String json = response.body();
-        return json;
+    public String obterDadosComStream(String endpoint) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endpoint))
+                .GET()
+                .build();
+
+        HttpResponse<InputStream> response = httpClient.send(request, HttpResponse.BodyHandlers.ofInputStream());
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(response.body()))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
     }
 }
+
